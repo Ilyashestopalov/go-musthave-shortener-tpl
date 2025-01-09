4647ef888723bae9064774cd6276aac07212d5ce
Ilya <ianr@yandex-team.ru>
2025-01-09 10:21:43 +0300

    f1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", ShortenerURL)
+
+	// Run server
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/app/handlers/handlers.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func ShortenerURL(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+}
