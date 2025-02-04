be435f765b3ff96f81471aefa8e12495592b5f00
Ilya <ianr@yandex-team.ru>
2025-02-04 15:53:58 +0300

    xxx

--- internal/app/links/shorten_url_test.go
+++ internal/app/links/shorten_url_test.go
@@ -1,43 +1,141 @@
 package links
 
 import (
-	"bytes"
+	"errors"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
-	"github.com/gin-gonic/gin"
+	"github.com/aseptimu/url-shortener/internal/app/service"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func setupRouter() *gin.Engine {
-	gin.SetMode(gin.ReleaseMode)
-	router := gin.Default()
-	router.POST("/", ShortenURLHandler)
-	router.GET("/:short_url", RedirectHandler)
-	return router
+func mockShortenURL(url string) (string, error) {
+	if url == "http://example.com" {
+		return "abcdef", nil
+	}
+	return "", errors.New("invalid URL format")
 }
 
-func TestShortenURLHandler(t *testing.T) {
-	router := setupRouter()
+func TestURLCreator(t *testing.T) {
+	service.ShortenURL = mockShortenURL
 
-	// Test valid request
-	longURL := "https://www.example.com"
-	req, _ := http.NewRequest(http.MethodPost, "/", bytes.NewBuffer([]byte(longURL)))
-	req.Header.Set("Content-Type", "text/plain")
+	type want struct {
+		code    int
+		method  string
+		body    string
+		isError bool
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "successful URL creation",
+			want: want{
+				code:    http.StatusCreated,
+				method:  http.MethodPost,
+				body:    "http://localhost:8080/abcdef",
+				isError: false,
+			},
+		},
+		{
+			name: "method not allowed",
+			want: want{
+				code:    http.StatusMethodNotAllowed,
+				method:  http.MethodGet,
+				body:    "Only POST method allowed\n",
+				isError: true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := httptest.NewRequest(tt.want.method, "/", strings.NewReader("http://example.com"))
 
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
+			w := httptest.NewRecorder()
+			URLCreator(w, r)
 
-	assert.Equal(t, http.StatusCreated, w.Code)
+			res := w.Result()
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
 
-	// Test invalid request
-	invalidLongURL := ""
-	req, _ = http.NewRequest(http.MethodPost, "/", bytes.NewBuffer([]byte(invalidLongURL)))
-	req.Header.Set("Content-Type", "text/plain")
+			if !tt.want.isError {
+				require.NoError(t, err)
+			}
 
-	w = httptest.NewRecorder()
-	router.ServeHTTP(w, req)
+			require.NotEmpty(t, resBody)
+			assert.Equal(t, res.StatusCode, tt.want.code)
+			assert.Equal(t, tt.want.body, string(resBody))
+		})
+	}
+}
 
-	assert.Equal(t, http.StatusBadRequest, w.Code)
+func mockGetOriginalURL(input string) (string, bool) {
+	if input != "/abcdef" {
+		return "http://example.com", true
+	}
+	return "", false
+}
+
+func TestGetURL(t *testing.T) {
+	service.GetOriginalURL = mockGetOriginalURL
+
+	type want struct {
+		code    int
+		method  string
+		url     string
+		isError bool
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "successful response",
+			want: want{
+				code:    http.StatusTemporaryRedirect,
+				method:  http.MethodGet,
+				url:     "/abcdef",
+				isError: false,
+			},
+		},
+		{
+			name: "wrong method",
+			want: want{
+				code:    http.StatusMethodNotAllowed,
+				method:  http.MethodPatch,
+				url:     "/abcdef",
+				isError: true,
+			},
+		},
+		{
+			name: "empty path",
+			want: want{
+				code:    http.StatusMethodNotAllowed,
+				method:  http.MethodPatch,
+				url:     "/",
+				isError: true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := httptest.NewRequest(tt.want.method, tt.want.url, nil)
+
+			w := httptest.NewRecorder()
+
+			GetURL(w, r)
+
+			res := w.Result()
+
+			defer res.Body.Close()
+
+			require.NotEmpty(t, res.Body)
+			assert.Equal(t, res.StatusCode, tt.want.code)
+		})
+	}
 }
